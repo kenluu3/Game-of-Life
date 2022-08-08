195d10cee905a8783f14a4a62a1c4815edfd35cb
kenluu3 <ken.luuu@hotmail.com>
2022-08-08 18:57:52 -0400

    adding render logic and update state logic

--- gol.py
+++ gol.py
@@ -1,10 +1,31 @@
+import pygame as pg
 from grid import Grid
 
 class GameOfLife:
-  def __init__(self, dimensions=(60,80)):
-    self.grid = Grid(dimensions[0], dimensions[1])
-    self.active = False
+  def __init__(self, cell_size=20, screen_size=(1280, 720)):
+    pg.init()
     
+    self.__colors = {
+      'bg': (255, 255, 255),
+      'dead': (25, 25, 25),
+      'alive_active': (0, 255, 0),
+      'alive_inactive': (255, 0, 0),
+    }
+    
+    self.__screen_size = screen_size
+    self.__cell_size = cell_size
+    self.__graphics = pg.display.set_mode(screen_size)
+    self.__grid = Grid(screen_size[1] // cell_size, screen_size[0] // cell_size)
+    self.__active = False
+    
+    pg.display.set_caption("Conway's Game of Life")
+  
+  def __del__(self):
+    pg.quit()
+  
+  def state(self):
+    return self.__grid
+
   def __neighbors(self, row, col):
     total_neighbors = 0
     
@@ -18,22 +39,38 @@ class GameOfLife:
       new_row = neighbor_row + row
       new_col = neighbor_col + col
       
-      if new_row >= 0 and new_row < self.grid.rows and new_col >= 0 and new_col < self.grid.cols:
-        if self.grid.get(new_row, new_col) == 1:
+      if new_row >= 0 and new_row < self.__grid.rows and new_col >= 0 and new_col < self.__grid.cols:
+        if self.__grid.get(new_row, new_col) == 1:
           total_neighbors += 1
           
     return total_neighbors
   
-  def update(self):
-    prev_state = self.grid.copy()
+  def __update(self):    
+    prev_state = self.__grid.copy()
     
-    for row in range(self.grid.rows()):
-      for col in range(self.grid.cols()):
+    for row in range(self.__grid.rows()):
+      for col in range(self.__grid.cols()):
         neighbors = self.__neighbors(row, col)
         
         if prev_state[row, col] == 1:
           if neighbors < 2 or neighbors > 3:
-            self.grid.set(row, col, 0)
+            self.__grid.set(row, col, 0)
         else:
           if neighbors == 3:
-            self.grid.set(row, col, 1)
+            self.__grid.set(row, col, 1)
+  
+  def __render(self):
+    self.__graphics.fill(self.__colors['bg'])
+    
+    for row in range(self.grid.rows()):
+      for col in range(self.grid.cols()):
+        render_rect = (row * self.__cell_size, col * self.__cell_size, self.__cell_size - 1, self.__cell_size - 1)
+        color = self.__colors['dead']
+        
+        if self.__grid.get(row, col) == 1:
+          if self.__active:
+            color = self.__colors['alive_active']
+          else:
+            color = self.__colors['alive_inactive']
+        
+        pg.draw.rect(self.__graphics, render_rect, color)  
