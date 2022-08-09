db0e890c7e73278b23b777d0f2a5d8ed53d24c4d
kenluu3 <ken.luuu@hotmail.com>
2022-08-08 23:21:14 -0400

    update member variable naming

--- grid.py
+++ grid.py
@@ -2,30 +2,30 @@ from copy import deepcopy
 
 class Grid:
   def __init__(self, rows=0, cols=0):
-    self.rows = rows
-    self.cols = cols
-    self.matrix = [[0] * cols for _ in range(rows)]  
+    self.__rows = rows
+    self.__cols = cols
+    self.__matrix = [[0] * cols for _ in range(rows)]  
 
   def set(self, row, col, value):
-    if row >= 0 and row < self.rows and col >= 0 and col < self.cols:
-      self.matrix[row, col] = value
+    if row >= 0 and row < self.__rows and col >= 0 and col < self.__cols:
+      self.__matrix[row, col] = value
   
   def get(self, row, col):
-    if row >= 0 and row < self.rows and col >= 0 and col < self.cols:
-      return self.matrix[row, col]
+    if row >= 0 and row < self.__rows and col >= 0 and col < self.__cols:
+      return self.__matrix[row, col]
   
   def copy(self):
-    return deepcopy(self.matrix)
+    return deepcopy(self.__matrix)
 
   def cols(self):
-    return self.cols
+    return self.__cols
 
   def rows(self):
-    return self.rows
+    return self.__rows
     
   def print_grid(self):
-    for row in range(self.rows):
-      for col in range(self.cols):
-        print(self.matrix[row][col], end=' ')
+    for row in range(self.__rows):
+      for col in range(self.__cols):
+        print(self.__matrix[row][col], end=' ')
       
       print()
